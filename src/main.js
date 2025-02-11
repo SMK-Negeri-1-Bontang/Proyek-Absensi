8f4b58112ab922ede03d3599f003af897ac731b4
TristanARM <tristanarmindo@gmail.com>
2025-02-11 08:31:52 +0800

    Install Axios via npm
    Axios buat mempermudah fetching API

--- src/main.js
+++ src/main.js
@@ -4,6 +4,11 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+// import Vue from 'vue'   // in Vue 2
+import * as Vue from 'vue' // in Vue 3
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
 const app = createApp(App)
 
 app.use(router)
