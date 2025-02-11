0f678700cd9dd149f8d4759402e7633cf014e38e
ValentinesNonbvore <valenpratamasahedi@gmail.com>
2025-02-11 08:41:44 +0800

    Membenarkan kesalahan Khimi

--- src/views/SplashscreenView.vue
+++ src/views/SplashscreenView.vue
@@ -3,18 +3,10 @@
 </script>
 
 <template>
-          <html>
-
-          <head>
-                    <script src="https://cdn.tailwindcss.com"></script>
-          </head>
-
-          <body class="bg-gray-900 flex items-center justify-center min-h-screen">
+          <div class="bg-gray-900 flex items-center justify-center min-h-screen">
                     <div class="text-center">
                               <p class="text-white text-2xl mb-4">You are...</p>
                               <p class="text-green-500 text-6xl font-bold">PRESENT</p>
                     </div>
-          </body>
-
-          </html>
+          </div>
 </template>
