9f724196ab91f93c55cdd98a4c1d78b05efb9b9b
ValentinesNonbvore <valenpratamasahedi@gmail.com>
2025-02-11 08:29:20 +0800

    menambahkan ui splash screen dan dashbord

--- src/views/SplashscreenView.vue
+++ src/views/SplashscreenView.vue
@@ -3,5 +3,18 @@
 </script>
 
 <template>
-          <h1 class="text-6xl">Selamat datang di laman Splashscreen</h1>
+          <html>
+
+          <head>
+                    <script src="https://cdn.tailwindcss.com"></script>
+          </head>
+
+          <body class="bg-gray-900 flex items-center justify-center min-h-screen">
+                    <div class="text-center">
+                              <p class="text-white text-2xl mb-4">You are...</p>
+                              <p class="text-green-500 text-6xl font-bold">PRESENT</p>
+                    </div>
+          </body>
+
+          </html>
 </template>
