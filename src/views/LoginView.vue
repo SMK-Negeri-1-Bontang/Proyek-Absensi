a7d61add97d995d0cdf6618999fb677767219b39
TristanARM <tristanarmindo@gmail.com>
2025-02-12 08:26:59 +0800

    Update LoginView.vue

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -14,16 +14,20 @@ const login = () => {
             <h1 class="text-2xl font-bold text-center mb-6 text-white">Selamat datang di laman Login</h1>
             <form @submit.prevent="login">
                 <div class="mb-4">
+                    <label class="block text-gray-400 text-sm font-bold mb-2" for="name">Nama</label>
+                    <input class="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 text-gray-100" type="text" placeholder="Masukkan nama" required>
+                </div>
+                <div class="mb-4">
                     <label class="block text-gray-400 text-sm font-bold mb-2" for="nis">NIS</label>
                     <input class="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 text-gray-100" type="text" placeholder="Masukkan NIS" required>
                 </div>
                 <div class="mb-6">
-                    <label class="block text-gray-400 text-sm font-bold mb-2" for="password">Password</label>
-                    <input  class="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 text-gray-100" type="password" placeholder="Masukkan Password" required>
+                    <label class="block text-gray-400 text-sm font-bold mb-2" for="password">Kata Sandi</label>
+                    <input  class="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 text-gray-100" type="password" placeholder="Masukkan kata sandi" required>
                 </div class="select-none">
                 <div class="flex flex-row gap-2">
-                    <div class="mb-6 grow">
-                    <label class="block text-gray-400 text-sm font-bold mb-2" for="class">Class</label>
+                    <div class="mb-6">
+                    <label class="block text-gray-400 text-sm font-bold mb-2" for="class">Kelas</label>
                         <select name="class" class="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 text-gray-100 bg-gray-800" required>
                             <option value="X">X</option>
                             <option value="XI">XI</option>
@@ -31,16 +35,17 @@ const login = () => {
                             <option value="XIII">XIII</option>
                         </select>
                     </div>
-                    <div class="mb-6 grow">
-                    <label class="block text-gray-400 text-sm font-bold mb-2" for="subdivisi">Subdivisi</label>
-                        <select name="subdivisi" class="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 text-gray-100 bg-gray-800" required>
+                    <div class="mb-6">
+                    <label class="block text-gray-400 text-sm font-bold mb-2" for="subdivision">Subdivisi</label>
+                        <select name="subdivision" class="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 text-gray-100 bg-gray-800" required>
+                            <option value="none"></option>
                             <option value="A">A</option>
                             <option value="B">B</option>
                         </select>
                     </div>
-                    <div class="mb-6 grow">
-                    <label class="block text-gray-400 text-sm font-bold mb-2" for="major">Major</label>
-                        <select name="class" class="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 text-gray-100 bg-gray-800" required>
+                    <div class="mb-6">
+                    <label class="block text-gray-400 text-sm font-bold mb-2" for="major">Jurusan</label>
+                        <select name="major" class="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 text-gray-100 bg-gray-800" required>
                             <option value="KA">Kimia Analis</option>
                             <option value="KI">Kimia Industri</option>
                             <option value="OI">Otomasi Industri</option>
@@ -54,7 +59,7 @@ const login = () => {
                         </select>
                     </div>
                 </div>
-                <button class="w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg" type="submit">Login</button>
+                <button class="w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg" type="submit">Masuk</button>
             </form>
         </div>
     </div>
