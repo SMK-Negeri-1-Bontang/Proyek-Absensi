fac734cb6cc93e76c65d7811213610f981afcccb
NayaKhiranyAprilia <nayabtg2020@gmail.com>
2025-02-12 08:13:01 +0800

    Page Not Found

--- src/views/NotFoundView.vue
+++ src/views/NotFoundView.vue
@@ -1,7 +1,10 @@
 <script setup>
-
 </script>
 
 <template>
-          <h1 class="text-6xl">Error 404: page not found.</h1>
+  <div class="flex justify-center items-center min-h-screen text-white bg-[linear-gradient(to_top_left,#734190,#734190,#4a77e0,#4a77e0,#7c95ff,#7c95ff)]">
+    <div class="text-center">
+      <p class="text-white-500 text-6xl font-bold">Error 404: page not found.</p>
+    </div>
+  </div>
 </template>
