5cde8c8312b503831471721dfddbdb7f2b83b7c0
TristanARM <tristanarmindo@gmail.com>
2025-02-12 09:21:06 +0800

    Pindahkan beberapa fitur dari login ke sign-up

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -18,47 +18,9 @@ const login = () => {
                     <label class="block text-gray-400 text-sm font-bold mb-2" for="name">Nama</label>
                     <input class="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 text-gray-100" type="text" placeholder="Masukkan nama" required>
                 </div>
-                <div class="mb-4">
-                    <label class="block text-gray-400 text-sm font-bold mb-2" for="nis">NIS</label>
-                    <input class="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 text-gray-100" type="text" placeholder="Masukkan NIS" required>
-                </div>
                 <div class="mb-6">
                     <label class="block text-gray-400 text-sm font-bold mb-2" for="password">Kata Sandi</label>
                     <input  class="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 text-gray-100" type="password" placeholder="Masukkan kata sandi" required>
-                </div class="select-none">
-                <div class="flex flex-row gap-2">
-                    <div class="mb-6">
-                    <label class="block text-gray-400 text-sm font-bold mb-2" for="class">Kelas</label>
-                        <select name="class" class="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 text-gray-100 bg-gray-800" required>
-                            <option value="X">X</option>
-                            <option value="XI">XI</option>
-                            <option value="XII">XII</option>
-                            <option value="XIII">XIII</option>
-                        </select>
-                    </div>
-                    <div class="mb-6">
-                    <label class="block text-gray-400 text-sm font-bold mb-2" for="subdivision">Subdivisi</label>
-                        <select name="subdivision" class="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 text-gray-100 bg-gray-800" required>
-                            <option value="none"></option>
-                            <option value="A">A</option>
-                            <option value="B">B</option>
-                        </select>
-                    </div>
-                    <div class="mb-6">
-                    <label class="block text-gray-400 text-sm font-bold mb-2" for="major">Jurusan</label>
-                        <select name="major" class="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 text-gray-100 bg-gray-800" required>
-                            <option value="KA">Kimia Analis</option>
-                            <option value="KI">Kimia Industri</option>
-                            <option value="OI">Otomasi Industri</option>
-                            <option value="PPLG">Pembangkitan Perangkat Lunak & Game</option>
-                            <option value="TPTU">Tata Pendinginan Udara</option>
-                            <option value="TL">Listrik</option>
-                            <option value="TKR">Kendaraan Ringan</option>
-                            <option value="LAS">Pengelasan</option>
-                            <option value="FAR">Farmasi</option>
-                            <option value="MES">Mesin</option>
-                        </select>
-                    </div>
                 </div>
                 <button class="w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg" type="submit">Masuk</button>
             </form>
--- src/views/SignupView.vue
+++ src/views/SignupView.vue
@@ -9,19 +9,58 @@ const login = () => {
 </script>
 
 <template>
-    <div class="flex items-center justify-center h-screen bg-gray-900">
-        <div class="w-full max-w-md bg-white p-8 rounded-lg shadow-lg">
-            <h1 class="text-2xl font-bold text-center mb-6">Selamat datang di laman Sign Up</h1>
+    <div class="flex items-center justify-center h-screen bg-[image:url('/src/components/images/kde_mountain.png')] bg-cover">
+        <div class="w-full max-w-md bg-gray-800 p-8 rounded-lg shadow-lg">
+            <img src="/src/components/images/Logo.png" alt="logo" class="h-16 mx-auto"> <!-- mx-auto mungkin merusakkan sesuatu-->
+            <h1 class="text-2xl font-bold text-center mb-6 text-white">Sign-up</h1>
             <form @submit.prevent="login">
                 <div class="mb-4">
-                    <label class="block text-gray-700 text-sm font-bold mb-2" for="nis">NIS</label>
-                    <input v-model="nis" class="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500" type="text" placeholder="Masukkan NIS" required>
+                    <label class="block text-gray-400 text-sm font-bold mb-2" for="name">Nama</label>
+                    <input class="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 text-gray-100" type="text" placeholder="Masukkan nama" required>
+                </div>
+                <div class="mb-4">
+                    <label class="block text-gray-400 text-sm font-bold mb-2" for="nis">NIS</label>
+                    <input class="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 text-gray-100" type="text" placeholder="Masukkan NIS" required>
                 </div>
                 <div class="mb-6">
-                    <label class="block text-gray-700 text-sm font-bold mb-2" for="password">Password</label>
-                    <input v-model="password" class="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500" type="password" placeholder="Masukkan Password" required>
+                    <label class="block text-gray-400 text-sm font-bold mb-2" for="password">Kata Sandi</label>
+                    <input  class="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 text-gray-100" type="password" placeholder="Masukkan kata sandi" required>
+                </div class="select-none">
+                <div class="flex flex-row gap-2">
+                    <div class="mb-6">
+                    <label class="block text-gray-400 text-sm font-bold mb-2" for="class">Kelas</label>
+                        <select name="class" class="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 text-gray-100 bg-gray-800" required>
+                            <option value="X">X</option>
+                            <option value="XI">XI</option>
+                            <option value="XII">XII</option>
+                            <option value="XIII">XIII</option>
+                        </select>
+                    </div>
+                    <div class="mb-6">
+                    <label class="block text-gray-400 text-sm font-bold mb-2" for="subdivision">Subdivisi</label>
+                        <select name="subdivision" class="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 text-gray-100 bg-gray-800" required>
+                            <option value="none"></option>
+                            <option value="A">A</option>
+                            <option value="B">B</option>
+                        </select>
+                    </div>
+                    <div class="mb-6">
+                    <label class="block text-gray-400 text-sm font-bold mb-2" for="major">Jurusan</label>
+                        <select name="major" class="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 text-gray-100 bg-gray-800" required>
+                            <option value="KA">Kimia Analis</option>
+                            <option value="KI">Kimia Industri</option>
+                            <option value="OI">Otomasi Industri</option>
+                            <option value="PPLG">Pembangkitan Perangkat Lunak & Game</option>
+                            <option value="TPTU">Tata Pendinginan Udara</option>
+                            <option value="TL">Listrik</option>
+                            <option value="TKR">Kendaraan Ringan</option>
+                            <option value="LAS">Pengelasan</option>
+                            <option value="FAR">Farmasi</option>
+                            <option value="MES">Mesin</option>
+                        </select>
+                    </div>
                 </div>
-                <button class="w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg" type="submit">Login</button>
+                <button class="w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg" type="submit">Masuk</button>
             </form>
         </div>
     </div>
