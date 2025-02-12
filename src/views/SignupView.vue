3370f0427ea319e15bc8d44ccf6b99372efacd71
ValentinesNonbvore <valenpratamasahedi@gmail.com>
2025-02-12 09:54:02 +0800

    Penerapan form dan v-model dalam sign up

--- src/views/SignupView.vue
+++ src/views/SignupView.vue
@@ -1,52 +1,80 @@
 <script setup>
-import { ref } from 'vue';
+import { reactive } from 'vue';
 
-const nis = ref('');
-const password = ref('');
-const login = () => {
-    console.log(`Login dengan NIS: ${nis.value}, Password: ${password.value}`);
+const form = reactive({
+    nama: '',
+    nis: '',
+    password: '',
+    kelas: 0,
+    subdivisi: '',
+    jurusan: ''
+});
+const signup = () => {
+    console.log(form);
 };
 </script>
 
 <template>
-    <div class="flex items-center justify-center h-screen bg-[image:url('/src/components/images/kde_mountain.png')] bg-cover">
+    <div
+        class="flex items-center justify-center h-screen bg-[image:url('/src/components/images/kde_mountain.png')] bg-cover">
         <div class="w-full max-w-md bg-gray-800 p-8 rounded-lg shadow-lg">
-            <img src="/src/components/images/Logo.png" alt="logo" class="h-16 mx-auto"> <!-- mx-auto mungkin merusakkan sesuatu-->
+            <img src="/src/components/images/Logo.png" alt="logo" class="h-16 mx-auto">
+            <!-- mx-auto mungkin merusakkan sesuatu-->
             <h1 class="text-2xl font-bold text-center mb-6 text-white">Sign-up</h1>
-            <form @submit.prevent="login">
+
+
+            <form @submit.prevent="signup">
+
                 <div class="mb-4">
-                    <label class="block text-gray-400 text-sm font-bold mb-2" for="name">Nama</label>
-                    <input class="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 text-gray-100" type="text" placeholder="Masukkan nama" required>
+                    <label class="block text-gray-400 text-sm font-bold mb-2" for="nama">Nama</label>
+                    <input
+                        class="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 text-gray-100"
+                        type="text" placeholder="Masukkan nama" id="nama" name="nama" required v-model="form.nama">
                 </div>
+
                 <div class="mb-4">
                     <label class="block text-gray-400 text-sm font-bold mb-2" for="nis">NIS</label>
-                    <input class="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 text-gray-100" type="text" placeholder="Masukkan NIS" required>
+                    <input
+                        class="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 text-gray-100"
+                        type="text" placeholder="Masukkan NIS" id="nis" name="nis" required v-model="form.nis">
                 </div>
+
                 <div class="mb-6">
                     <label class="block text-gray-400 text-sm font-bold mb-2" for="password">Kata Sandi</label>
-                    <input  class="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 text-gray-100" type="password" placeholder="Masukkan kata sandi" required>
+                    <input
+                        class="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 text-gray-100"
+                        type="password" placeholder="Masukkan kata sandi" id="password" name="password" required v-model="form.password">
                 </div class="select-none">
+
                 <div class="flex flex-row gap-2">
                     <div class="mb-6">
-                    <label class="block text-gray-400 text-sm font-bold mb-2" for="class">Kelas</label>
-                        <select name="class" class="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 text-gray-100 bg-gray-800" required>
-                            <option value="X">X</option>
-                            <option value="XI">XI</option>
-                            <option value="XII">XII</option>
-                            <option value="XIII">XIII</option>
+                        <label class="block text-gray-400 text-sm font-bold mb-2" for="kelas">Kelas</label>
+                        <select name="kelas"
+                            class="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 text-gray-100 bg-gray-800" id="kelas"
+                            required v-model="form.kelas">
+                            <option :value="1">X</option>
+                            <option :value="2">XI</option>
+                            <option :value="3">XII</option>
+                            <option :value="4">XIII</option>
                         </select>
                     </div>
+
                     <div class="mb-6">
-                    <label class="block text-gray-400 text-sm font-bold mb-2" for="subdivision">Subdivisi</label>
-                        <select name="subdivision" class="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 text-gray-100 bg-gray-800" required>
+                        <label class="block text-gray-400 text-sm font-bold mb-2" for="subdivisi">Subdivisi</label>
+                        <select name="subdivisi"
+                            class="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 text-gray-100 bg-gray-800" id="subdivisi"
+                            required v-model="form.subdivisi">
                             <option value="none"></option>
                             <option value="A">A</option>
                             <option value="B">B</option>
                         </select>
                     </div>
+
                     <div class="mb-6">
-                    <label class="block text-gray-400 text-sm font-bold mb-2" for="major">Jurusan</label>
-                        <select name="major" class="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 text-gray-100 bg-gray-800" required>
+                        <label class="block text-gray-400 text-sm font-bold mb-2" for="jurusan">Jurusan</label>
+                        <select name="jurusan"
+                            class="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 text-gray-100 bg-gray-800" id="jurusan"
+                            required v-model="form.jurusan">
                             <option value="KA">Kimia Analis</option>
                             <option value="KI">Kimia Industri</option>
                             <option value="OI">Otomasi Industri</option>
@@ -59,8 +87,10 @@ const login = () => {
                             <option value="MES">Mesin</option>
                         </select>
                     </div>
+
                 </div>
-                <button class="w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg" type="submit">Masuk</button>
+                <button class="w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg"
+                    type="submit">Masuk</button>
             </form>
         </div>
     </div>
