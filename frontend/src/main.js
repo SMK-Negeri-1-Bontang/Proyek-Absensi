54ee17410128fb69a80f814d05ed3de313120391
ValentinesNonbvore <valenpratamasahedi@gmail.com>
2025-03-03 19:31:13 +0800

    fix main.js

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,11 +1,10 @@
+import './assets/main.css'
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import { faSignOutAlt, faArrowRight } from "@fortawesome/free-solid-svg-icons";
 
-// Tambahkan ikon ke library FontAwesome
 library.add(faSignOutAlt, faArrowRight);
 
-// Registrasi komponen FontAwesome secara global di Vue
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
@@ -14,14 +13,3 @@ const app = createApp(App);
 app.component("font-awesome-icon", FontAwesomeIcon);
 app.use(router);
 app.mount("#app");
-
-
-library.add(faSignOutAlt, faArrowRight);
-
-const app = createApp(App)
-
-app.component('font-awesome-icon', FontAwesomeIcon);
-
-app.use(router)
-
-app.mount('#app')
