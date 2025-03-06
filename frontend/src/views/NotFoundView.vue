f39a6cecb9b8a031f8fb21dadbc6cfea1ca1dcbc
IsThatKhim <isqimyup@gmail.com>
2025-03-06 19:27:56 +0800

    perbaikan source

--- frontend/src/views/NotFoundView.vue
+++ frontend/src/views/NotFoundView.vue
@@ -4,7 +4,7 @@
 <template>
   <div class="flex justify-center items-center min-h-screen text-white bg-[linear-gradient(to_top_left,#734190,#734190,#4a77e0,#4a77e0,#7c95ff,#7c95ff)]">
     <div class="text-center">
-      <img src="/images/logo.png" alt="Logo" class="w-32 h-auto mx-auto mb-4">
+      <img src="@/components/images/Logo.png" alt="Logo" class="w-32 h-auto mx-auto mb-4">
       <p class="text-white text-6xl font-bold">Error 404: Page Not Found.</p>
     </div>
   </div>
