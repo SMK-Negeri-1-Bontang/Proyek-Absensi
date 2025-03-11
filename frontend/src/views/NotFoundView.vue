06bc13cbef9871428e6ccfe95850425ddc24badf
IsThatKhim <isqimyup@gmail.com>
2025-03-11 15:41:56 +0800

    remake font and penempatan logo

--- frontend/src/views/NotFoundView.vue
+++ frontend/src/views/NotFoundView.vue
@@ -4,8 +4,9 @@
 <template>
   <div class="flex justify-center items-center min-h-screen text-white bg-gray-800">
     <div class="text-center">
-      <img src="@/components/images/Logo.png" alt="Logo" class="w-32 h-auto mx-auto mb-4">
-      <p class="text-white text-50xl font-bold">404: Page Not Found</p>
+      <img src="@/components/images/Logo.png" alt="Logo" class="w-70 h-auto mx-auto mb-[-40px]">
+      <p class="text-white text-[170px] font-bold">404</p>
+      <p class="text-white text-4xl mt-[-50px]">Page Not Found</p>
     </div>
   </div>
 </template>
