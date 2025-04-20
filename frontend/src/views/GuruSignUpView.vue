b2abcb4db5fc8b0f30ad05779d34f65f26389775
IsThatKhim <isqimyup@gmail.com>
2025-04-21 07:34:54 +0800

    fix some error

--- frontend/src/views/GuruSignUpView.vue
+++ frontend/src/views/GuruSignUpView.vue
@@ -47,83 +47,94 @@ const signup = async () => {
 </script>
 
 <template>
-          <div :class="['flex items-center justify-center sm:py-[150px] min-h-screen', isSmallScreen ? 'bg-gray-800' : `bg-[url('@/components/images/kde_mountain.png')] bg-cover `]">
-                    <div class="w-full max-w-full bg-gray-800 p-8 pt-16 rounded-lg sm:shadow-lg   sm:max-w-md">
-                              <h1 class="text-2xl font-bold text-center mb-6 text-white">
-                                        Sign up Guru
-                              </h1>
+    <div :class="['flex items-center justify-center sm:py-[150px] min-h-screen', isSmallScreen ? 'bg-gray-800' : `bg-[url('@/components/images/kde_mountain.png')] bg-cover`]">
+        <div class="w-full max-w-full bg-gray-800 p-8 pt-16 rounded-lg sm:shadow-lg sm:max-w-md">
+            <img src="@/components/images/Logo.png" alt="logo" class="h-16 mx-auto" />
+            <h1 class="text-2xl font-bold text-center mb-6 text-white">
+                Sign up Guru
+            </h1>
 
-                              <form @submit.prevent="signup">
-                                        <div class="mb-4">
-                                                  <label class="block text-gray-400 text-sm font-bold mb-2"
-                                                            for="nama">Nama</label>
-                                                  <input class="w-full px-4 py-2 border rounded-lg" type="text"
-                                                            id="nama" v-model="form.nama
-                                                                      .content
-                                                                      " />
-                                                  <p v-if="
-                                                            form.nama
-                                                                      .error
-                                                  " class="text-red-500 text-sm mt-2">
-                                                            {{ form.nama.error }}
-                                                  </p>
-                                        </div>
+            <form @submit.prevent="signup">
+                <div class="mb-4">
+                    <label class="block text-gray-400 text-sm font-bold mb-2" for="nama">Nama</label>
+                    <input
+                        class="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 text-gray-100"
+                        type="text" id="nama" placeholder="Masukkan nama" v-model="form.nama
+                            .content
+                            " />
+                    <p v-if="
+                        form.nama
+                            .error
+                    " class="text-red-500 text-sm mt-2">
+                        {{ form.nama.error }}
+                    </p>
+                </div>
 
-                                        <div class="mb-4">
-                                                  <label class="block text-gray-400 text-sm font-bold mb-2"
-                                                            for="nip">NIP</label>
-                                                  <input class="w-full px-4 py-2 border rounded-lg" type="text" id="nip"
-                                                            v-model="form.nip
-                                                                      .content
-                                                                      " />
-                                                  <p v-if="
-                                                            form.nip
-                                                                      .error
-                                                  " class="text-red-500 text-sm mt-2">
-                                                            {{ form.nip.error }}
-                                                  </p>
-                                        </div>
+		<div class="mb-4">
+                    <label class="block text-gray-400 text-sm font-bold mb-2" for="nip">NIP</label>
+                    <input
+                        class="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 text-gray-100"
+                        id="nip" placeholder="Masukkan NIP" v-model="form.nip
+                            .content
+                            " />
+                    <p v-if="
+                        form.nip
+                            .error
+                    " class="text-red-500 text-sm mt-2">
+                        {{ form.nip.error }}
+                    </p>
+                </div>
 
-                                        <div class="mb-4">
-                                                  <label class="block text-gray-400 text-sm font-bold mb-2"
-                                                            for="password">Kata Sandi</label>
-                                                  <input class="w-full px-4 py-2 border rounded-lg" type="password"
-                                                            id="password" v-model="form
-                                                                      .password
-                                                                      .content
-                                                                      " />
-                                                  <p v-if="
-                                                            form
-                                                                      .password
-                                                                      .error
-                                                  " class="text-red-500 text-sm mt-2">
-                                                            {{ form.password.error }}
-                                                  </p>
-                                        </div>
+		<div class="mb-4">
+                    <label class="block text-gray-400 text-sm font-bold mb-2" for="password">Kata Sandi</label>
+                    <input
+                        class="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 text-gray-100"
+                        type="password" placeholder="Masukkan kata sandi" id="password" v-model="form
+                            .password
+                            .content
+                            " />
+                    <p v-if="
+                        form
+                            .password
+                            .error
+                    " class="text-red-500 text-sm mt-2">
+                        {{ form.password.error }}
+                    </p>
+                </div>
 
-                                        <div class="mb-6">
-                                                  <label class="block text-gray-400 text-sm font-bold mb-2"
-                                                            for="confirmPassword">Konfirmasi Kata
-                                                            Sandi</label>
-                                                  <input class="w-full px-4 py-2 border rounded-lg" type="password"
-                                                            id="confirmPassword" v-model="form
-                                                                      .confirmPassword
-                                                                      .content
-                                                                      " />
-                                                  <p v-if="
-                                                            form
-                                                                      .confirmPassword
-                                                                      .error
-                                                  " class="text-red-500 text-sm mt-2">
-                                                            {{ form.confirmPassword.error }}
-                                                  </p>
-                                        </div>
+		<div class="mb-2">
+                    <label class="block text-gray-400 text-sm font-bold mb-2" for="confirmPassword">Konfirmasi Kata
+                        Sandi</label>
+                    <input
+                        class="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 text-gray-100"
+                        type="password" id="confirmPassword" placeholder="Konfirmasi kata sandi" v-model="form
+                            .confirmPassword
+                            .content
+                            " />
+                    <p v-if="
+                        form
+                            .confirmPassword
+                            .error
+                    " class="text-red-500 text-sm mt-2">
+                        {{ form.confirmPassword.error }}
+                    </p>
+                </div>
 
-                                        <button class="w-full bg-blue-500 text-white font-bold py-2 px-4 rounded-lg cursor-pointer"
-                                                  type="submit">
-                                                  Daftar
-                                        </button>
-                              </form>
-                    </div>
-          </div>
+		<p class="mb-6 text-sm">
+                    <RouterLink to="/" class="text-white">Log in
+                        <font-awesome-icon :icon="[
+                            'fas',
+                            'arrow-right',
+                        ]" class="text-white" />
+                    </RouterLink>
+                </p>
+
+		<button
+                    class="w-full bg-[linear-gradient(to_right,#734190,#734190,#4a77e0,#4a77e0,#7c95ff,#7c95ff)] hover:bg-[linear-gradient(to_right,#9161b0,#5f8af0,#9db3ff)] text-white font-bold py-2 px-4 rounded-lg cursor-pointer"
+                    type="submit">
+                    Daftar
+                </button>
+            </form>
+        </div>
+    </div>
 </template>
