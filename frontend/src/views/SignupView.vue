69ec70e4865e0c3fba723db30159156ae60306ba
IsThatKhim <isqimyup@gmail.com>
2025-04-21 07:37:52 +0800

    fix some error

--- frontend/src/views/SignupView.vue
+++ frontend/src/views/SignupView.vue
@@ -90,7 +90,7 @@ watch(
     <!--<pre>{{ JSON.stringify(users.siswa, null, 2) }}</pre>-->
 
     <div
-        class:class="['flex items-center justify-center sm:py-[150px] min-h-screen', isSmallScreen ? 'bg-gray-800' : `bg-[url('@/components/images/kde_mountain.png')] bg-cover `]">
+    :class="['flex items-center justify-center sm:py-[150px] min-h-screen', isSmallScreen ? 'bg-gray-800' : `bg-[url('@/components/images/kde_mountain.png')] bg-cover `]">
         <div class="w-full max-w-full bg-gray-800 p-8 pt-16 rounded-lg sm:shadow-lg   sm:max-w-md">
             <img src="@/components/images/Logo.png" alt="logo" class="h-16 mx-auto" />
             <h1 class="text-2xl font-bold text-center mb-6 text-white">
